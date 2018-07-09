79eb6337e120bd04fc2b0ef53a93b6c4cf553b8d
Elon <22001029@qq.com>
2018-07-10 07:09:40 +0800

    上传配置

--- DynamicDataSource/src/main/java/com/elon/dds/App.java
+++ DynamicDataSource/src/main/java/com/elon/dds/App.java
@@ -1,5 +1,7 @@
 package com.elon.dds;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,9 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App 
 {
+    private static Logger log = LogManager.getLogger(App.class);
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         SpringApplication.run(App.class, args);
+        log.info("Start dynamic data source success.");
     }
 }
